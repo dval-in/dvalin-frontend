4ac94f2db4047fcd61188b72b7fcb2956bd31801
Pigeon <94313401+TerrifiedPigeon@users.noreply.github.com>
2024-01-07 12:26:27 -0500

    fix: Wording of import export buttons in settings

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -3,56 +3,55 @@ import { tv, type VariantProps } from 'tailwind-variants';
 import type { Button as ButtonPrimitive } from 'bits-ui';
 
 const buttonVariants = tv({
-    base: 'inline-flex items-center rounded-md text-sm font-medium whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 group',
-    variants: {
-        variant: {
-            default: 'justify-center bg-primary text-primary-foreground hover:bg-primary/90',
-            destructive:
-                'justify-center bg-destructive text-destructive-foreground hover:bg-destructive/90',
-            outline:
-                'justify-center border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-            secondary: 'justify-center bg-secondary text-secondary-foreground hover:bg-secondary/80',
-            ghost: 'justify-center hover:bg-accent hover:text-accent-foreground',
-            link: 'justify-center text-primary underline-offset-4 hover:underline',
-            sidebar:
-                'bg-sidebar justify-center ml-0 text-sidebarText lg:justify-start lg:ml-8 hover:text-white hover:bg-primary/90 w-full',
-            activeSidebar:
-                'bg-activeSidebar justify-center ml-0 text-sidebarText/110 lg:justify-start lg:ml-8 hover:text-white hover:bg-primary/90 w-full',
-            headerSidebar:
-                'justify-center bg-black/10 text-sidebarText/80 w-full lg:justify-start hover:text-white',
-            navigatorClose: 'justify-center bg-transparent'
-        },
-        size: {
-            default: 'h-10 px-4 py-2',
-            sm: 'h-9 rounded-md px-3',
-            lg: 'h-11 rounded-md px-8',
-            icon: 'h-10 w-10'
-        }
-    },
-    defaultVariants: {
-        variant: 'default',
-        size: 'default'
-    }
+	base: 'inline-flex items-center rounded-md text-sm font-medium whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 group',
+	variants: {
+		variant: {
+			default: 'justify-center bg-primary text-primary-foreground hover:bg-primary/90',
+			destructive:
+				'justify-center bg-destructive text-destructive-foreground hover:bg-destructive/90',
+			outline:
+				'justify-center border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+			secondary: 'justify-center bg-secondary text-secondary-foreground hover:bg-secondary/80',
+			ghost: 'justify-center hover:bg-accent hover:text-accent-foreground',
+			link: 'justify-center text-primary underline-offset-4 hover:underline',
+			sidebar:
+				'bg-sidebar justify-center ml-0 text-sidebarText lg:justify-start lg:ml-8 hover:text-white hover:bg-primary/90 w-full',
+			activeSidebar:
+				'bg-activeSidebar justify-center ml-0 text-sidebarText/110 lg:justify-start lg:ml-8 hover:text-white hover:bg-primary/90 w-full',
+			headerSidebar:
+				'justify-center bg-black/10 text-sidebarText/80 w-full lg:justify-start hover:text-white',
+			navigatorClose: 'justify-center bg-transparent'
+		},
+		size: {
+			default: 'h-10 px-4 py-2',
+			sm: 'h-9 rounded-md px-3',
+			lg: 'h-11 rounded-md px-8',
+			icon: 'h-10 w-10'
+		}
+	},
+	defaultVariants: {
+		variant: 'default',
+		size: 'default'
+	}
 });
 
 type Variant = VariantProps<typeof buttonVariants>['variant'];
 type Size = VariantProps<typeof buttonVariants>['size'];
 
 type Props = ButtonPrimitive.Props & {
-    variant?: Variant;
-    size?: Size;
+	variant?: Variant;
+	size?: Size;
 };
 
 type Events = ButtonPrimitive.Events;
 
 export {
-    Root,
-    type Props,
-    type Events,
-    //
-    Root as Button,
-    type Props as ButtonProps,
-    type Events as ButtonEvents,
-    buttonVariants
+	Root,
+	type Props,
+	type Events,
+	//
+	Root as Button,
+	type Props as ButtonProps,
+	type Events as ButtonEvents,
+	buttonVariants
 };
-
