1fcc092300dd7dacc01c448dd2b166c3a158faba
XenosG <x.e.n.o.s.s.g.g@gmail.com>
2024-01-07 17:29:23 +0200

    feat: added more cards

--- src/routes/homepage/+page.svelte
+++ src/routes/homepage/+page.svelte
@@ -3,44 +3,96 @@
 	import { Button } from '$lib/components/ui/button';
 
 	let changeLog = `Version 4.3:
-〓 Update Details 〓`;
+
+New Characters
+- 5-Star Character "Helm of the Radiant Rose" Navia (Geo)
+  - Vision: Geo
+  - Weapon: Claymore
+  - Elemental Skill: Ceremonial Crystalshot
+  - Elemental Burst: As the Sunlit Sky's Singing Salute
+
+- 4-Star Character "Executor of Justice" Chevreuse (Pyro)
+  - Vision: Pyro
+  - Weapon: Polearm
+  - Elemental Skill: Short-Range Rapid Interdiction Fire
+  - Elemental Burst: Ring of Bursting Grenades
+
+`;
 </script>
 
 <div>
-	<div class="flex flex-row gap-x-3">
+	<div class="grid grid-cols-4 gap-4 auto-rows-fr">
 		<!-- Welcome card -->
-		<div class="flex-1">
-			<Card.Root class="text-white">
+		<div class="col-span-2 row-span-1">
+			<Card.Root class="size-full text-white flex flex-col">
 				<Card.Header>
 					<!--TODO: Replace this with i18n key-->
 					<Card.Title>Welcome to Dval.in!</Card.Title>
 				</Card.Header>
-				<Card.Content>
+				<Card.Content class="flex-1">
 					<p>
 						Your best Genshin Impact companion! Dval.in helps you plan what to farm with an
 						ascension calculator, and it also tracks your progress with a todo list and a wish
 						counter.
 					</p>
 				</Card.Content>
-				<Card.Footer class="justify-end">
+				<Card.Footer class="flex-1 items-end justify-end">
 					<Button class="rounded-xl border-2 border-gray-600">Hide</Button>
 				</Card.Footer>
 			</Card.Root>
 		</div>
 		<!-- Changelog card -->
-		<div class="flex-1">
-			<Card.Root class="text-white flex flex-col">
+		<div class="col-span-2 row-span-2">
+			<Card.Root class="size-full text-white flex flex-col">
 				<Card.Header>
 					<!--TODO: Replace this with i18n key-->
 					<Card.Title>Changelog:</Card.Title>
 				</Card.Header>
-				<Card.Content class="flex-1 truncate">
+				<Card.Content class="flex-1">
 					<p class="whitespace-pre-line">{changeLog}</p>
 				</Card.Content>
 				<Card.Footer class="flex-1 items-end justify-end">
 					<Button class="rounded-xl border-2 border-gray-600">More</Button>
+					<Button class="rounded-xl border-2 border-gray-600 ml-5">Hide</Button>
 				</Card.Footer>
 			</Card.Root>
 		</div>
+		<!-- Reminder card -->
+		<div class="align-start">
+			<Card.Root class="size-full text-white">
+				<Card.Header>
+					<!--TODO: Replace this with i18n key-->
+					<Card.Title>Reminder</Card.Title>
+				</Card.Header>
+				<Card.Content>
+					<p class="whitespace-pre-line"></p>
+				</Card.Content>
+			</Card.Root>
+		</div>
+		<!-- Events card -->
+		<div class="align-start">
+			<Card.Root class="size-full text-white">
+				<Card.Header>
+					<!--TODO: Replace this with i18n key-->
+					<Card.Title>Current Events</Card.Title>
+				</Card.Header>
+				<Card.Content>
+					<p class="whitespace-pre-line"></p>
+				</Card.Content>
+			</Card.Root>
+		</div>
+		<!-- To-do card -->
+		<div class="align-start">
+			<Card.Root class="size-full text-white">
+				<Card.Header>
+					<!--TODO: Replace this with i18n key-->
+					<Card.Title>To-do List</Card.Title>
+				</Card.Header>
+				<Card.Content>
+					<p class="whitespace-pre-line"></p>
+				</Card.Content>
+			</Card.Root>
+		</div>
 	</div>
+
 </div>
