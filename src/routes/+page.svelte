dd8deaa7a026f1d8a142eb5613b778f406f7835c
Pigeon <94313401+TerrifiedPigeon@users.noreply.github.com>
2024-01-07 15:51:50 -0500

    fix: Button and Coloring QoLs and hovering problems
    Co-Authored-By: MasterLegend2772 <dingxy2772@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,23 @@
-src/routes/homepage/+page.svelte
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+	import { Button } from '$lib/components/ui/button';
+	import { settings } from '$lib/store/global_state';
+	import { get } from 'svelte/store';
+	import { type Settings, Convert as SettingsConvertor } from '$lib/structs/settings';
+</script>
+      
+<div class="col-span-2 row-span-2">
+         <Card.Root class="size-full text-white flex flex-col">
+             <Card.Header>
+                 <!--TODO: Replace this with i18n key-->
+                 <Card.Title>Changelog:</Card.Title>
+             </Card.Header>
+             <Card.Content class="flex-1">
+                 <p class="whitespace-pre-line">Changelog TXT</p>
+             </Card.Content>
+             <Card.Footer class="flex-1 items-end justify-end">
+                 <Button variant="default" class="rounded-xl border-2 border-gray-600">More</Button>
+                 <Button variant="link" class="rounded-xl border-2 border-gray-600 ml-5">Hide</Button>
+             </Card.Footer>
+         </Card.Root>
+</div>
