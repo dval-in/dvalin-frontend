64fd0313a72ce0bc90e77eefecb66b82b76cfb13
XenosG <x.e.n.o.s.s.g.g@gmail.com>
2024-01-08 02:54:14 +0200

    feat: setup masonry layout for cards

--- src/routes/homepage/+page.svelte
+++ src/routes/homepage/+page.svelte
@@ -20,79 +20,102 @@ New Characters
 `;
 </script>
 
-<div>
-	<div class="grid grid-cols-4 gap-4 auto-rows-fr">
+<div class="">
+	<section class="flex flex-row justify-between px-5 items-center mb-5">
+		<h1 class="text-4xl font-semibold">Home</h1>
+		<Button class="rounded-xl border-2 border-gray-600">Edit</Button>
+	</section>
+	<div
+		class="h-dvh [column-count:3] [column-width:20em] gap-2 [&>div:not(:first-child)]:mt-2 [&>div:not(:first-child)]:break-inside-avoid"
+	>
 		<!-- Welcome card -->
-		<div class="col-span-2 row-span-1">
-			<Card.Root class="size-full text-white flex flex-col">
-				<Card.Header>
-					<!--TODO: Replace this with i18n key-->
-					<Card.Title>Welcome to Dval.in!</Card.Title>
-				</Card.Header>
-				<Card.Content class="flex-1">
-					<p>
-						Your best Genshin Impact companion! Dval.in helps you plan what to farm with an
-						ascension calculator, and it also tracks your progress with a todo list and a wish
-						counter.
-					</p>
-				</Card.Content>
-				<Card.Footer class="flex-1 items-end justify-end">
-					<Button class="rounded-xl border-2 border-gray-600">Hide</Button>
-				</Card.Footer>
-			</Card.Root>
-		</div>
+		<Card.Root class="break-inside-avoid text-white flex flex-col">
+			<Card.Header>
+				<!--TODO: Replace this with i18n key-->
+				<Card.Title>Welcome to Dval.in!</Card.Title>
+			</Card.Header>
+			<Card.Content class="flex-1">
+				<p>
+					Your best Genshin Impact companion! Dval.in helps you plan what to farm with an ascension
+					calculator, and it also tracks your progress with a todo list and a wish counter.
+				</p>
+			</Card.Content>
+			<Card.Footer class="flex-1 items-end justify-end">
+				<Button class="rounded-xl border-2 border-gray-600">Hide</Button>
+			</Card.Footer>
+		</Card.Root>
 		<!-- Changelog card -->
-		<div class="col-span-2 row-span-2">
-			<Card.Root class="size-full text-white flex flex-col">
-				<Card.Header>
-					<!--TODO: Replace this with i18n key-->
-					<Card.Title>Changelog:</Card.Title>
-				</Card.Header>
-				<Card.Content class="flex-1">
-					<p class="whitespace-pre-line">{changeLog}</p>
-				</Card.Content>
-				<Card.Footer class="flex-1 items-end justify-end">
-					<Button class="rounded-xl border-2 border-gray-600">More</Button>
-					<Button class="rounded-xl border-2 border-gray-600 ml-5">Hide</Button>
-				</Card.Footer>
-			</Card.Root>
-		</div>
+		<Card.Root class="text-white flex flex-col">
+			<Card.Header>
+				<!--TODO: Replace this with i18n key-->
+				<Card.Title>Changelog:</Card.Title>
+			</Card.Header>
+			<Card.Content class="flex-1">
+				<p class="whitespace-pre-line">{changeLog}</p>
+			</Card.Content>
+			<Card.Footer class="flex-1 items-end justify-end">
+				<Button variant="default" class="rounded-xl border-2 border-gray-600">More</Button>
+			</Card.Footer>
+		</Card.Root>
 		<!-- Reminder card -->
-		<div class="align-start">
-			<Card.Root class="size-full text-white">
-				<Card.Header>
-					<!--TODO: Replace this with i18n key-->
-					<Card.Title>Reminder</Card.Title>
-				</Card.Header>
-				<Card.Content>
-					<p class="whitespace-pre-line"></p>
-				</Card.Content>
-			</Card.Root>
-		</div>
+		<Card.Root class="text-white">
+			<Card.Header>
+				<!--TODO: Replace this with i18n key-->
+				<Card.Title>Reminder</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<p class="whitespace-pre-line"></p>
+			</Card.Content>
+		</Card.Root>
 		<!-- Events card -->
-		<div class="align-start">
-			<Card.Root class="size-full text-white">
-				<Card.Header>
-					<!--TODO: Replace this with i18n key-->
-					<Card.Title>Current Events</Card.Title>
-				</Card.Header>
-				<Card.Content>
-					<p class="whitespace-pre-line"></p>
-				</Card.Content>
-			</Card.Root>
-		</div>
+		<Card.Root class="text-white">
+			<Card.Header>
+				<!--TODO: Replace this with i18n key-->
+				<Card.Title>Current Events</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<p class="whitespace-pre-line"></p>
+			</Card.Content>
+		</Card.Root>
 		<!-- To-do card -->
-		<div class="align-start">
-			<Card.Root class="size-full text-white">
-				<Card.Header>
-					<!--TODO: Replace this with i18n key-->
-					<Card.Title>To-do List</Card.Title>
-				</Card.Header>
-				<Card.Content>
-					<p class="whitespace-pre-line"></p>
-				</Card.Content>
-			</Card.Root>
-		</div>
+		<Card.Root class="text-white">
+			<Card.Header>
+				<!--TODO: Replace this with i18n key-->
+				<Card.Title>To-do List</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<p class="whitespace-pre-line"></p>
+			</Card.Content>
+		</Card.Root>
+		<!-- Achievements card -->
+		<Card.Root class="text-white">
+			<Card.Header>
+				<!--TODO: Replace this with i18n key-->
+				<Card.Title>Achievements</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<p class="whitespace-pre-line"></p>
+			</Card.Content>
+		</Card.Root>
+		<!-- Pity card -->
+		<Card.Root class="text-white">
+			<Card.Header>
+				<!--TODO: Replace this with i18n key-->
+				<Card.Title>Pity</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<p class="whitespace-pre-line"></p>
+			</Card.Content>
+		</Card.Root>
+		<!-- Display card -->
+		<Card.Root class="text-white">
+			<Card.Header>
+				<!--TODO: Replace this with i18n key-->
+				<Card.Title>Display</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<p class="whitespace-pre-line"></p>
+			</Card.Content>
+		</Card.Root>
 	</div>
-
 </div>
