4de7cedc31f51f65f289ff565a89e4d784136a77
Ludovic Malot <ludovicmalot@icloud.com>
2024-01-23 16:27:04 +0100

    fix: using a more standard approach to env variable

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,9 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/core/providers/github';
-import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
+
+const GITHUB_ID = process.env.GITHUB_ID;
+const GITHUB_SECRET = process.env.GITHUB_SECRET;
 
 export const handle = SvelteKitAuth({
-	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })]
+    providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })]
 });
