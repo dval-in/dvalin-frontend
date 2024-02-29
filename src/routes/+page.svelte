cd5a101d403365eaac3a61f918f48ea8677d24fd
Alexander König <mail@koenig-alexander.dev>
2024-02-29 19:03:02 +0100

    refactor: changelog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,21 +12,24 @@
 				<Card.Title>Changelog:</Card.Title>
 			</Card.Header>
 			<Card.Content class="flex-1">
-				<Text type="p"
-					>Improved import (support a wider range of exports, no multi account though!!)</Text
-				>
+				<Text type="p">
+					Improved import (support a wider range of exports, no multi account though!!)
+				</Text>
 				<Text type="p">Beginner's banner</Text>
 				<Text type="p">
 					Improved pull list (added pull number, highlight 4 & 5 star, filter by dates)
 				</Text>
-				<Text type="p"
-					>Fixed images (no brokey anymore!!!!, huge thanks to @Flappydom for helping with renaming
+				<Text type="p">
+					Fixed images (no brokey anymore!!!!, huge thanks to @Flappydom for helping with renaming
 					files)
 				</Text>
 				<Text type="p">Improved search experience</Text>
 				<Text type="p">Characters are retrieved from user data</Text>
 				<Text type="p">
 					Use GOOD types under the hood (You cant see it but believe me... its there)
+				</Text>
+				<Text type="p">
+					Removed random rarity in preparation for character/weapon/rarity mapping
 				</Text>
 			</Card.Content>
 			<Card.Footer class="flex-1 items-end gap-5 justify-end">
