5fe36242063e28a45986deda126973b5f8f264b1
maalni <mail@koenig-alexander.dev>
2024-03-30 22:08:48 +0100

    refactor: cleanup (#75)
    * refactor: cleanup code
    
    * refactor: remove empty .npmrc
    
    * refactor: remove unnecessary secrets
    
    * refactor: remove wrangler dependency

--- src/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-import { SvelteKitAuth } from '@auth/sveltekit';
-import GitHub from '@auth/core/providers/github';
-
-const GITHUB_ID = process.env.GITHUB_ID;
-const GITHUB_SECRET = process.env.GITHUB_SECRET;
-
-export const handle = SvelteKitAuth({
-	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })]
-});
