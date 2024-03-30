dc9784db26601c9c66dfc3db58230060ca07a51d
Alexander König <mail@koenig-alexander.dev>
2024-03-30 21:46:49 +0100

    refactor: cleanup code

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,15 +1,5 @@
 <script lang="ts">
-	import { signIn, signOut } from '@auth/sveltekit/client';
-	import { page } from '$app/stores';
-	import { Button } from '$lib/components/ui/button';
+	import DefaultLayout from '$lib/components/layout/DefaultLayout.svelte';
 </script>
 
-{#if $page.data.session?.user}
-	<p>Signed in as {$page.data.session.user.email}</p>
-	<Button on:click={() => signOut}>Sign Out</Button>
-{:else}
-	<p style="font-size: 21px"><b><u>Not Signed in</u></b></p>
-	<br />
-
-	<Button on:click={() => signIn('github')}>Github</Button>
-{/if}
+<DefaultLayout title="Login">Soon</DefaultLayout>
