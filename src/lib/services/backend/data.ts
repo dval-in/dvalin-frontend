c4af0f009c3db4bcc6d0ee3745d9d3b5b646b484
Sasikuttan2163 <abhinav.anil.2k5@gmail.com>
2024-07-08 20:43:07 +0530

    feat: read Achievement Categories in frontend

--- src/lib/services/backend/data.ts
+++ src/lib/services/backend/data.ts
@@ -8,10 +8,14 @@ import type { CharacterIndex } from '$lib/types/index/character';
 import type { WeaponIndex } from '$lib/types/index/weapon';
 import type { Character } from '$lib/types/data/Character';
 import type { Weapon } from '$lib/types/data/Weapon';
+import type { AchievementCategoryIndex } from '$lib/types/index/achievementcategory';
+import type { AchievementCategoryKey } from '$lib/types/keys/AchievementCategoryKey';
+import type { AchievementCategory } from '$lib/types/data/AchievementCategory';
 
 interface FetchDataIndexResponse {
 	character: CharacterIndex;
 	weapon: WeaponIndex;
+	achievementCategory: AchievementCategoryIndex;
 }
 
 export class BackendDataService {
@@ -36,10 +40,14 @@ export class BackendDataService {
 					const weaponIndex = await backendFetch<WeaponIndex>(
 						`${this.baseUrl}/Weapon/index?lang=${get(i18n).language}`
 					);
-
+					const achievementCategoryIndex = await backendFetch<AchievementCategoryIndex>(
+						`${this.baseUrl}/AchievementCategory/index?lang=${get(i18n).language}`
+					);
+					console.log(achievementCategoryIndex);
 					return {
 						character: charIndex,
-						weapon: weaponIndex
+						weapon: weaponIndex,
+						achievementCategory: achievementCategoryIndex
 					};
 				}
 			},
@@ -58,7 +66,22 @@ export class BackendDataService {
 			this.queryClient
 		);
 	}
-
+	fetchAchievementCategoryData(achievementCategory: AchievementCategoryKey) {
+		return createQuery<AchievementCategory>(
+			{
+				queryKey: ['fetchAchievementCategoryData', achievementCategory],
+				staleTime: 6 * 60 * 60 * 1000, //6h
+				queryFn: async () =>
+					await backendFetch<AchievementCategory>(
+						this.getAchievementCategoryDataUrl(achievementCategory)
+					)
+			},
+			this.queryClient
+		);
+	}
+	getAchievementCategoryDataUrl(achievementCategory: AchievementCategoryKey) {
+		return `${this.baseUrl}/AchievementCategory/${achievementCategory}?lang=${get(i18n).language}`;
+	}
 	getCharacterDataUrl(character: CharacterKey) {
 		return `${this.baseUrl}/Character/${character}?lang=${get(i18n).language}`;
 	}
--- src/lib/store/index_store.ts
+++ src/lib/store/index_store.ts
@@ -1,10 +1,16 @@
 import { persisted } from 'svelte-persisted-store';
 import type { CharacterIndex } from '$lib/types/index/character';
 import type { WeaponIndex } from '$lib/types/index/weapon';
+import type { AchievementCategoryIndex } from '$lib/types/index/achievementcategory';
 
-const defaultValues: { character: CharacterIndex; weapon: WeaponIndex } = {
+const defaultValues: {
+	character: CharacterIndex;
+	weapon: WeaponIndex;
+	achievementCategory: AchievementCategoryIndex;
+} = {
 	character: {},
-	weapon: {}
+	weapon: {},
+	achievementCategory: {}
 };
 
 export const dataIndexStore = persisted('dataIndex', defaultValues, {
