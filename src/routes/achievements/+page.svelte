aacb3a1ea5ba08ee6bc2bbdecb70391265d9c968
Sasikuttan2163 <abhinav.anil.2k5@gmail.com>
2024-07-22 11:32:01 +0530

    feat: generate achievement category cards

--- src/routes/achievements/+page.svelte
+++ src/routes/achievements/+page.svelte
@@ -5,37 +5,39 @@
 	import DefaultLayout from '$lib/components/layout/DefaultLayout.svelte';
 	import { dataIndexStore } from '$lib/store/index_store';
 	import i18n from '$lib/services/i18n';
+	import type { AchievementCategoryKey } from '$lib/types/keys/AchievementCategoryKey';
+	import AchievementCategoryCard from '$lib/components/ui/card/AchievementCategoryCard.svelte';
 
 	const transformedAchievements = derived([dataIndexStore], ([dataIndexStore]) => {
-		const achievements = dataIndexStore.achievementCategory;
-		console.log(achievements);
-		return achievements;
+		const achievements = Object.keys(dataIndexStore.achievementCategory).map((key) => {
+			const achievement = dataIndexStore.achievementCategory[key as AchievementCategoryKey];
+			return {
+				link: `/achievement/${key}`,
+				name: achievement!.name,
+				order: achievement!.order,
+				total: achievement!.total
+			};
+		});
+		return achievements.sort((a, b) => a.order - b.order);
 	});
 
-	console.log(transformedAchievements);
 	console.log(get(applicationState));
 	console.log(get(userProfile));
 </script>
 
 <!-- svelte-ignore a11y-no-noninteractive-element-to-interactive-role -->
 <!-- svelte-ignore a11y-label-has-associated-control -->
-<DefaultLayout title={$i18n.t('characters.overview.title')}>
-	<svelte:fragment slot="titlebarActions">
-		<!--		<Searchbar searchGroup="Characters" searchableDataList={$transformedAchievements} />-->
-	</svelte:fragment>
-	<div class="flex flex-wrap gap-4 justify-center">
-		<!--{#each $transformedAchievements as achievement}-->
-		<!--	<CharCard-->
-		<!--		link={character.link}-->
-		<!--		name={character.name}-->
-		<!--		img={character.img}-->
-		<!--		level={0}-->
-		<!--		constellation={0}-->
-		<!--		element={character.element}-->
-		<!--		weapon={character.weapon}-->
-		<!--		rarity={character.rarity}-->
-		<!--		obtained={character.obtained}-->
-		<!--	/>-->
-		<!--{/each}-->
+<DefaultLayout title={$i18n.t('achievements.categories.title')}>
+	<svelte:fragment slot="titlebarActions"></svelte:fragment>
+	<div class="flex flex-wrap gap-3 justify-center">
+		{#each $transformedAchievements as achievement}
+			<AchievementCategoryCard
+				link={achievement.link}
+				name={achievement.name}
+				img=""
+				total={achievement.total}
+				achieved={achievement.total}
+			/>
+		{/each}
 	</div>
 </DefaultLayout>
