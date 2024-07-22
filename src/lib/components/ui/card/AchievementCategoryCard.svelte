ce048af3eff10e2cf0e3e6383c866c735bacc973
Sasikuttan2163 <abhinav.anil.2k5@gmail.com>
2024-07-22 11:32:01 +0530

    feat: generate achievement category cards

--- src/lib/components/ui/card/AchievementCategoryCard.svelte
+++ src/lib/components/ui/card/AchievementCategoryCard.svelte
@@ -1,24 +1,32 @@
 <script lang="ts">
 	import Text from '$lib/components/typography/Text.svelte';
+	import { Progress } from 'svelte-ux';
 
 	export let link: string;
 	export let name: string;
 	export let img: string;
 	export let total: number;
 	export let achieved: number;
+	let percent = (achieved / total) * 100;
 </script>
 
 <a
-	class="flex flex-col w-24 xl:w-36 rounded-md overflow-hidden hover:scale-105 transition-transform duration-300 ease-in-out"
+	class="flex flex-col w-40 xl:w-52 rounded-md overflow-hidden bg-tertiary/30 hover:scale-105 transition-transform duration-300 ease-in-out"
 	href={link}
 >
-	<div class={`flex justify-center`}>
-		<img alt={name} class="w-24 h-24 xl:w-36 xl:h-36" src={img} />
+	<div class="flex flex-col p-2 text-center">
+		<div class={`flex justify-center`}>
+			<img alt={name} class="w-24 h-24 xl:w-36 xl:h-36" src={img} />
+		</div>
+		<div class="hidden">{link} {name} {total} {achieved}</div>
 	</div>
-	<div class="flex flex-1 justify-center p-2 text-center bg-tertiary items-center">
+	<div class={'text-center justify-center content-end mx-3 my-3'}>
+		<Text type="small">
+			{achieved}/{total} ({percent}%)
+		</Text>
+		<Progress value={percent} />
 		<Text type="h4">
 			{name}
 		</Text>
-		<div class="hidden">{link} {name} {total} {achieved}</div>
 	</div>
 </a>
