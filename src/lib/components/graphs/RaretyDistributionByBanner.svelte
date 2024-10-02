f8f849022f07d7da04a3ba94b76b0bd34c0474a2
Alexander König <mail@koenig-alexander.dev>
2024-10-02 13:32:00 +0900

    fix: convert pie to arc chart

--- src/lib/components/graphs/RaretyDistributionByBanner.svelte
+++ src/lib/components/graphs/RaretyDistributionByBanner.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import { scaleOrdinal } from 'd3-scale';
-	import { Chart, Pie, Svg, Tooltip } from 'layerchart';
+	import { PieChart } from 'layerchart';
 	import type { IWish } from '$lib/types/wish';
 	import { derived, type Readable } from 'svelte/store';
 
@@ -10,6 +9,9 @@
 		const rarityCount: { [key: number]: number } = {};
 
 		wishDataStore.forEach((wish: IWish) => {
+			if (wish.rarity === 0) {
+				return;
+			}
 			if (rarityCount[wish.rarity] === undefined) {
 				rarityCount[wish.rarity] = 0;
 			}
@@ -24,44 +26,21 @@
 			.sort((a, b) => a.rarity - b.rarity);
 	});
 
-	const colorKeys = [3, 4, 5];
 	const keyColors = ['#5E93B2', '#7B5C90', '#FFB13F'];
 </script>
 
-<div class="h-[300px] min-w-[300px] w-full">
-	<Chart
+<div class="h-[300px] w-full">
+	<PieChart
 		data={$getData}
-		let:tooltip
-		r="rarity"
-		rDomain={colorKeys}
-		rRange={keyColors}
-		rScale={scaleOrdinal()}
-		x="value"
-	>
-		<Svg>
-			<Pie {tooltip} />
-		</Svg>
-		<Tooltip.Root class="bg-neutral" let:data>
-			<Tooltip.List>
-				<Tooltip.Item
-					format="integer"
-					label="Star"
-					value={data.rarity}
-					valueAlign="right"
-				/>
-				<Tooltip.Item
-					format="integer"
-					label="value"
-					value={data.value}
-					valueAlign="right"
-				/>
-				<Tooltip.Item
-					format="percent"
-					label="percent"
-					value={data.value / $wishData.length}
-					valueAlign="right"
-				/>
-			</Tooltip.List>
-		</Tooltip.Root>
-	</Chart>
+		label="rarity"
+		cRange={keyColors}
+		value="value"
+		outerRadius={300 / 2}
+		innerRadius={-20}
+		cornerRadius={10}
+		padAngle={0.02}
+		range={[-90, 90]}
+		props={{ group: { y: 80 } }}
+		legend
+	/>
 </div>
