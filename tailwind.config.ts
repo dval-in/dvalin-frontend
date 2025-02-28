2d19673afc1768047174bf73f732a57a36fd99b2
Alexander König <mail@koenig-alexander.dev>
2025-02-28 18:09:00 +0900

    chore: remove svelte ux and fix tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,5 @@
 import type { Config } from 'tailwindcss';
 
 export const config: Config = {
-	darkMode: 'selector',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		'./node_modules/svelte-ux/**/*.{svelte,js}',
-		'./node_modules/layerchart/**/*.{svelte,js}'
-	]
+	darkMode: 'selector'
 };
